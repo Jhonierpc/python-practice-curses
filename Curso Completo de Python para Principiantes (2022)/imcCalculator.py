23a03731fc498877f124cc421c9c7affd03f9171
Jhonierpc <jhonierrz@gmail.com>
2023-09-13 18:37:22 -0500

    Se realizan algunas modificaciones en el primer código de python y se agregan algunos archivos

--- Curso Completo de Python para Principiantes (2022)/imcCalculator.py
+++ Curso Completo de Python para Principiantes (2022)/imcCalculator.py
@@ -5,20 +5,25 @@
 # Entre 26 y 30: Sobrepeso
 # > 30: Obesidad
 
-peso = int(input('Ingrese su peso en kg: '))
-altura = int(input('Ingrese su altura en cm: '))
+def calcularIMC(peso, alturaEnMetros):
+    imc = peso / (alturaEnMetros * alturaEnMetros)
+    return imc
 
-altura = altura / 100
+def pedirIMC():
+    peso = float(input('Ingrese su peso en kg: '))
+    alturaEnCM = int(input('Ingrese su altura en cm: '))
+    alturaEnMetros = alturaEnCM / 100
+    imc = calcularIMC(peso, alturaEnMetros)
 
-imc = peso / (altura * altura)
+    print('Su IMC es de: ' + str(imc))
 
-print('Su IMC es de: ' + str(imc))
+    if imc < 20:
+        print('Estado de delgadez')
+    if 20 <= imc < 26:
+        print('Estado normal')
+    if 26 <= imc < 30:
+        print('Estado de sobrepeso')
+    if imc >= 30:
+        print('Estado de obesidad')
 
-if imc < 20:
-    print('Estado de delgadez')
-if 20 <= imc < 26:
-    print('Estado normal')
-if 26 <= imc < 30:
-    print('Estado de sobrepeso')
-if imc >= 30:
-    print('Estado de obesidad')
+pedirIMC()
